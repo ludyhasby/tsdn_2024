c3d0d8e63c57baa69c9dc5a6e3590879a6a0c422
Ludy Hasby Aulia <fewesgalih@gmail.com>
2024-11-13 11:37:08 +0700

    drop unnecassary file

--- save.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoModelForImageClassification
-
-efficientnet_model = AutoModelForImageClassification.from_pretrained("google/efficientnet-b7")
-efficientnet_model.save_pretrained("efficientnet-b7")
